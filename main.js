174d80e110c7b996786d338b69fdb349f551afcd
Briones-9E-ay2526 <briones.zc@obmontessori.edu.ph>
2026-04-17 10:31:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function openSesame(){
+
+    let carCounter = 10;
+    const maxCars = 30;
+
+    while (carCounter < maxCars){
+        window.alert("Car Number " + carCounter + " is entering");
+        carCounter += 1;
+    }
+
+    window.alert(Garageisfull)
+}
+
